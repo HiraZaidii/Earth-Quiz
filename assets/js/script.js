e3a47ccf710b2f38079e08cf37aed546e6d8a234
HiraZaidii <hirazaidi@hotmail.com>
2022-05-05 14:09:12 +0000

    Add questions to js script

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,38 +11,89 @@ let questionCounter = 0
 let availableQuestions = []
 
 let questions = [{
-        question: 'What is the answer?',
-        choice1: '',
-        choice2: '',
-        choice3: '',
+        question: 'What is thWhen you go to the grocery shop, it’s best to:',
+        choice1: '..buy a disposable bag at the counter to carry your groceries home',
+        choice2: '..bring your own reusable bag for your groceries every time',
         answer: 2,
     },
     {
-        question: 'What is the answer?',
-        choice1: '',
-        choice2: '',
-        choice3: '',
+        question: 'When you are out for a walk and get thirsty, it’s best to:',
+        choice1: '..bring your own reusable water bottle with you so you can drink',
+        choice2: '..buy a plastic water bottle from the store',
+        answer: 1,
+    },
+    {
+        question: 'When you are throwing away your paper drawings, it’s best to:',
+        choice1: '..throw it away separately in the paperbox',
+        choice2: '..throw it any nearest trash bin',
+        answer: 1,
+    },
+    {
+        question: 'When you don’t play with your old toys, it’s best to::',
+        choice1: '..to throw them away in the trash',
+        choice2: '..to donate it to other children',
         answer: 2,
     },
     {
-        question: 'What is the answer?',
-        choice1: '',
-        choice2: '',
-        choice3: '',
-        answer: 2,
-    },
-    {
-        question: 'What is the answer?',
-        choice1: '',
-        choice2: '',
-        choice3: '',
-        answer: 2,
-    },
-    {
-        question: 'What is the answer?',
-        choice1: '',
-        choice2: '',
-        choice3: '',
-        answer: 2,
-    },
+        question: 'When you wash your hands, it’s best to:',
+        choice1: '..to close the tap while soaping your hands',
+        choice2: '..to keep the tap running while you soap your hands ',
+        answer: 1,
+    }
 ]
+
+const SCORE_POINTS = 100
+const MAX_QUESTIONS = 4
+
+startGame = () => {
+    questionCounter = 0
+    score = 0
+    availableQuestions = (...questions)
+    getNewQuestion()
+}
+
+getNewQuestion = () => {
+    if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign('/end.html')
+    }
+
+    questionCounter++
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+    progressBarfull.getElementsByClassName.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`
+
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
+
+    acceptingAnswers = true
+}
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if (!acceptingAnswers) return
+
+        acceptingAnswers = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+
+        if (classToApply === 'correct') {
+            incrementScore(SCORE_POINTS)
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply)
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestion()
+        }, 1000)
+    })
+})
