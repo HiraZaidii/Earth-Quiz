79c6fac7c9b64677b379320143e660a0e355a199
HiraZaidii <hirazaidi@hotmail.com>
2022-05-09 16:04:54 +0000

    Correct id= prgressBarfull

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,60 +5,60 @@ const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progressBarFull');
 
-let currentQuestion = {}
-let acceptingAnswers = true
-let score = 0
-let questionCounter = 0
-let availableQuestions = []
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
 
 let questions = [{
-        question: 'When you go to the grocery shop, it’s best to..',
+        question: "When you go to the grocery shop, it’s best to..",
         choice1: '..buy a disposable bag at the counter to carry your groceries home',
         choice2: '..bring your own reusable bag for your groceries every time',
         answer: 2,
     },
     {
-        question: 'When you are out for a walk and get thirsty, it’s best to..',
+        question: "When you are out for a walk and get thirsty, it’s best to..",
         choice1: '..bring your own reusable water bottle with you so you can drink',
         choice2: '..buy a plastic water bottle from the store',
         answer: 1,
     },
     {
-        question: 'When you don’t play with your old toys, it’s best to..',
+        question: "When you don’t play with your old toys, it’s best to..",
         choice1: '..to throw them away in the trash',
         choice2: '..to donate it to other children',
         answer: 2,
     },
     {
-        question: 'When you wash your hands, it’s best to..',
+        question: "When you wash your hands, it’s best to..",
         choice1: '..to close the tap while soaping your hands',
         choice2: '..to keep the tap running while you soap your hands ',
         answer: 1,
     },
     {
-        question: 'When you are throwing away your paper, it’s best to..',
+        question: "When you are throwing away your paper, it’s best to..",
         choice1: '..throw it away separately in the paperbox',
         choice2: '..throw it any nearest trash bin',
         answer: 1,
     }
 ];
 
-const SCORE_POINTS = 100
-const MAX_QUESTIONS = 5
+const SCORE_POINTS = 100;
+const MAX_QUESTIONS = 5;
 
 startGame = () => {
     questionCounter = 0
     score = 0
     availableQuestions = [...questions]
     getNewQuestion()
-}
+};
 
 getNewQuestion = () => {
     if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score)
 
         return window.location.assign('/end.html')
-    }
+    };
 
     questionCounter++
     progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
@@ -107,4 +107,4 @@ incrementScore = num => {
     scoreText.innerText = score
 }
 
-startGame()
+startGame();
